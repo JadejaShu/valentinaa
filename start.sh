b4e057161126ad64ebb721c6bdee42dc53a7e5aa
JadejaShu <139604204+JadejaShu@users.noreply.github.com>
2024-11-07 16:41:08 +0530

    url update

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/JadejaShu/testrepo.git /testRepo
+  git clone https://github.com/JadejaShu/valentinaa.git /valentinaa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /valentinaa
 fi
-cd /LazyPrincess
+cd /valentinaa
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
